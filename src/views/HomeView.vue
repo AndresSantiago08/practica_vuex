7f04ebcdc027e4350e80963845ca0d567a2be185
AndreSantiago08 <harper_hska_5@hotmail.com>
2022-03-24 01:22:50 -0600

    Update: mostrar imagen de pastelería

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/Pasteleria.png">
+    <img alt="pasteleria logo" src="../assets/Pasteleria.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
