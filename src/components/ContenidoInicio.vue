51916d1028e2ebaf3b6f1b743e960d5696375838
AndreSantiago08 <harper_hska_5@hotmail.com>
2022-03-24 23:33:51 -0600

    Update: componente terminado

---
+++ src/components/ContenidoInicio.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <img alt="pastel chocolate" src="../assets/pastel.jpg" >
+    </div>
+    <hr>
+    <div>
+        <p>
+            Dirección: Av. Revolución 779, Nonoalco, Benito Juárez, 03700 Ciudad de México, CDMX<br>
+            Teléfono: 55 5555 5555<br>
+            Horario: Todos los días de 9:00 am a 9:30 pm
+        </p>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: "ContenidoInicio",
+    }
+</script>
+
+<style scoped>
+    div img{
+        position: relative;
+        margin: 20px;
+        border-radius: 10px;
+        width: 1000px;
+    }
+    div p{
+        font-size: 18px;
+    }
+</style>>
